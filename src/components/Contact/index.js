b5ad10b0a31ea434db20c28874392dd5b952759c
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-07-25 22:37:02 -0700

    add contact form and styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 function Contact() {
-  return(
+  return (
     <section>
-      <div>
-        <h2>Contact Me</h2>
-        <p>Contact information</p>
+      <div className="container">
+        <h2 className="row">Contact Me</h2>
+        <form className="row" id="contact-form">
+          <div className="mb-3">
+            <label htmlFor="name" className="form-label">Name:</label>
+            <input type="text" className="form-control" name="name"></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email" className="form-label">Email address:</label>
+            <input type="email" className="form-control" name="email"></input>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="message" className="form-label">Message:</label>
+            <textarea name="message" className="form-control" rows="5" />
+          </div>
+          <button data-testid="button" className="btn btn-outline-light" type="submit">Submit</button>
+        </form>
       </div>
     </section>
   )
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer>
+    <footer className='fixed-bottom'>
       <p>created by Jodee Ann Conui</p>
     </footer>
   )

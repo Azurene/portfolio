c546ad8966b9063f8da7fa5550af876465dbe011
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-08-15 13:33:11 -0700

    update routes to be compatible with gh pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -13,22 +13,22 @@ function Nav() {
       <nav className="link-list">
         <ul>
           <li className="btn btn-outline-light">
-            <Link to='/'>
+            <Link to='/portfolio'>
               About Me
             </Link>
           </li>
           <li>
-            <Link to='/portfolio' className="btn btn-outline-light">
+            <Link to='/portfolio/portfolio' className="btn btn-outline-light">
               Portfolio
             </Link>
           </li>
           <li>
-            <Link to='/contact' className="btn btn-outline-light">
+            <Link to='/portfolio/contact' className="btn btn-outline-light">
               Contact
             </Link>
           </li>
           <li>
-            <Link to='/resume' className="btn btn-outline-light">
+            <Link to='/portfolio/resume' className="btn btn-outline-light">
               Resume
             </Link>
           </li>

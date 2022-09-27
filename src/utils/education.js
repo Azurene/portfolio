237ac3ffb1ed71c825bcee4d481b4db6971c7c11
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2022-09-26 23:54:13 -0700

    added utils

---
+++ src/utils/education.js
@@ -0,0 +1,24 @@
+const education = [
+  {
+    location: "Moorpark College",
+    title: "A.A.-T Psychology",
+    time: "2022"
+  },
+  {
+    location: "Moorpark College",
+    title: "A.A. General Education - Social and Behavioral Sciences",
+    time: "2022"
+  },
+  {
+    location: "Moorpark College",
+    title: "A.A. General Education - Humanities",
+    time: "2019"
+  },
+  {
+    location: "Moorpark College",
+    title: "A.A. General Education - Natural Sciences",
+    time: "2017"
+  }
+]
+
+export default education;
---
+++ src/utils/work.js
@@ -0,0 +1,40 @@
+const work = [
+  {
+    jobTitle: "Community Service Assistant",
+    company: "CSUN Department of Police Services",
+    time: "September 2022 - current",
+    location: "Northridge, Los Angeles, CA"
+  },
+  {
+    jobTitle: "Research Assistant",
+    company: "Positive Augmented Research and Development Laboratory",
+    time: "May 2022 - current",
+    location: "Northridge, Los Angeles, CA"
+  },
+  {
+    jobTitle: "Online Student Support",
+    company: "Moorpark College",
+    time: "August 2021 - December 2021",
+    location: "Moorpark, CA"
+  },
+  {
+    jobTitle: "Cashier",
+    company: "Hobby Lobby",
+    time: "November 2022 - August 2021",
+    location: "Simi Valley, CA"
+  },
+  {
+    jobTitle: "Sales Associate",
+    company: "Dollar General",
+    time: "October 2019 - March 2020",
+    location: "Bristol, CT"
+  },
+  {
+    jobTitle: "Seasonal Sales Associate",
+    company: "Kohl's",
+    time: "2015",
+    location: "Simi Valley, CA"
+  },
+];
+
+export default work;

a9c312ba9b67aa5054e3215021768dbbc7dade2c
Jodee Ann Conui <98633770+Azurene@users.noreply.github.com>
2023-03-31 13:40:14 -0700

    conditional rendering of information component

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 const Information = (props) => {
 
+
   return (
     <article className='container'>
-      <div className='row'>
-        <p className='col'>{props.title}</p>
-        <p className='col'>{props.company}</p>
-        <p className='col'>{props.time}</p>
-        <p className='col'>{props.location}</p>
-        <p className='col'>{props.description}</p>
+       <div className='row'>
+        {props.title && <p className='col'>{props.title}</p>}
+        {props.company && <p className='col'>{props.company}</p>}
+        {props.title && <p className='col'>{props.time}</p>}
+        {props.location && <p className='col'>{props.location}</p>}
+        {props.description && <p className='col'>{props.description}</p>}
       </div>
     </article>
   )
--- src/utils/skill.js
+++ src/utils/skill.js
@@ -1,11 +1,11 @@
 const skill = [
   {
     skillName: "Programming Languages",
-    description: "HTML, CSS, JavaScript"
+    time: "HTML, CSS, JavaScript"
   },
   {
     skillName: "Full Stack Web Developer Bootcamp",
-    time: "Awarded August 2022"
+    time: "Awarded August 2022",
   },
   {
     skillName: "Human Subjects Research Basic Course",
